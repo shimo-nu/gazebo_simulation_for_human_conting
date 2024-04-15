f1baaca9921fdd5fc3ca75bd24e40ccd402af822
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2024-04-15 18:43:48 +0900

    add parameter

--- simulation/src/joy_twist_publisher.cpp
+++ simulation/src/joy_twist_publisher.cpp
@@ -18,15 +18,28 @@ public:
     linear_x_ = 0.0;
     linear_y_ = 0.0;
     angular_z_ = 0.0;
+
+
+    double gain_linear_x_ = 0.5;
+    double gain_angular_z_ = 0.5;
+    declare_parameter("gain_linear_x", 0.0);
+    declare_parameter("gain_angular_z", 0.0);
+    get_parameter("gain_linear_x", gain_linear_x_);
+    get_parameter("gain_angular_z", gain_angular_z_);
+
+    RCLCPP_INFO(this->get_logger(), "Parameter: Joy Twist Publisher");
+    RCLCPP_INFO(this->get_logger(), "gain_linear_x : %f", gain_linear_x_);
+    RCLCPP_INFO(this->get_logger(), "gain_angular_z : %f", gain_angular_z_);
+
   }
 
 private:
   void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
   {
     // Joyスティックの値に応じてパラメータを設定
-    linear_x_ = joy_msg->axes[1] * 0.5;  // y軸(-1.0 to 1.0)
+    linear_x_ = joy_msg->axes[1] * gain_linear_x_;  // y軸(-1.0 to 1.0)
     // linear_y_ = joy_msg->axes[0] * 0.5;  // x軸(-1.0 to 1.0)
-    angular_z_ = joy_msg->axes[2] * 0.5; // z軸(-1.0 to 1.0)
+    angular_z_ = joy_msg->axes[2] * gain_angular_z_; // z軸(-1.0 to 1.0)
 
     // cmd_velメッセージを作成
     geometry_msgs::msg::Twist twist_msg;
