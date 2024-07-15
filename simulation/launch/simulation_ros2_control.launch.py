bf901b19b091df354e8ae921abdb092b9c3f156d
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2024-07-16 00:18:20 +0900

    fix launch

--- simulation/launch/simulation_ros2_control.launch.py
+++ simulation/launch/simulation_ros2_control.launch.py
@@ -10,6 +10,77 @@ from launch.conditions import IfCondition
 import os
 import xacro
 
+def robot_settings(robot_name, robot_namespace):
+    node_list = []
+    # Load urdf
+    share_dir_path = os.path.join(get_package_share_directory('journal1-sim'))
+    xacro_path = os.path.join(share_dir_path, 'urdf', 'smagv', 'smagv.urdf.xacro')
+    urdf_path = os.path.join(share_dir_path, 'urdf', 'smagv', 'smagv.urdf')
+
+    doc = xacro.process_file(xacro_path)
+    robot_description = doc.toprettyxml(indent='  ')  
+    with open(urdf_path, 'w') as f:
+        f.write(robot_description)
+        
+    xml = robot_description.replace('"', '\\"')
+    # spawn_args = '{name: \"my_robot\", xml: \"' + xml + '\",robot_namespace: \"robot_1\"}'
+    spawn_args = '{name: \"' + robot_name + '\", xml: \"' + xml + '\", robot_namespace: \"' + robot_namespace + '\"}'
+
+
+    # Robot state publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}],
+        namespace=robot_namespace
+    )
+    
+    # Joint state publisher
+    joint_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen',
+        namespace=robot_namespace
+    )
+
+    
+    node_list.extend([robot_state_publisher_node, joint_publisher])
+    
+    
+    diff_drive_node = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "diff_drive_controller",
+        ],
+        output='screen',
+        namespace=robot_namespace
+    )
+    
+    
+    controller_manager = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[{'robot_description': robot_description}],
+        output='screen'
+    )
+    
+    
+    gazebo_robot_spawner = ExecuteProcess(
+            cmd=['ros2', 'service', 'call', '/spawn_entity',
+                'gazebo_msgs/SpawnEntity', spawn_args]),
+    
+    
+    node_list.append(gazebo_robot_spawner[0])
+    node_list.append(diff_drive_node)
+    # node_
+    
+    
+    return node_list
+
+
 def generate_launch_description():
     # Declare the launch arguments
     launch_arguments = []
@@ -18,6 +89,9 @@ def generate_launch_description():
     def add_launch_arg(name: str, default_value=None):
         launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
 
+
+   
+    
     
     add_launch_arg('show_rviz', 'true')
     add_launch_arg('use_sim_time', 'true')
@@ -41,17 +115,18 @@ def generate_launch_description():
     # spawn_args = '{name: \"my_robot\", xml: \"' + xml + '\" }'
 
 
-    share_dir_path = os.path.join(get_package_share_directory('journal1-sim'))
-    xacro_path = os.path.join(share_dir_path, 'urdf', 'smagv', 'smagv.urdf.xacro')
-    urdf_path = os.path.join(share_dir_path, 'urdf', 'smagv', 'smagv.urdf')
+    # share_dir_path = os.path.join(get_package_share_directory('journal1-sim'))
+    # xacro_path = os.path.join(share_dir_path, 'urdf', 'smagv', 'smagv.urdf.xacro')
+    # urdf_path = os.path.join(share_dir_path, 'urdf', 'smagv', 'smagv.urdf')
 
-    doc = xacro.process_file(xacro_path)
-    robot_description = doc.toprettyxml(indent='  ')  
-    with open(urdf_path, 'w') as f:
-        f.write(robot_description)
+    # doc = xacro.process_file(xacro_path)
+    # robot_description = doc.toprettyxml(indent='  ')  
+    # with open(urdf_path, 'w') as f:
+    #     f.write(robot_description)
         
-    xml = robot_description.replace('"', '\\"')
-    spawn_args = '{name: \"my_robot\", xml: \"' + xml + '\" }'
+    # xml = robot_description.replace('"', '\\"')
+    # # spawn_args = '{name: \"my_robot\", xml: \"' + xml + '\",robot_namespace: \"robot_1\"}'
+    # spawn_args = '{name: \"my_robot\", xml: \"' + xml + '\"}'
 
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -73,73 +148,8 @@ def generate_launch_description():
     
     node_list.extend([gzserver_cmd])
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_description}]
-    )
+    node_list.extend(robot_settings(robot_name='robot_1', robot_namespace='robot1',))
     
-    joint_publisher = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        output='screen'
-    )
-
-    
-    node_list.extend([robot_state_publisher_node, joint_publisher])
-
-    
-    joint_state_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        output='screen',
-        name='joint_state_controller',
-        arguments=['joint_state_controller'],
-    )
-    
-        
-    skid_steer_controller = Node(
-        package='controller_manager',
-        executable='spawner',
-        output='screen',
-        name='skid_steer_controller',
-        arguments=['skid_steer_controller', '-c', '/gazebo_ros2_control'],
-    )
-    
-    
-    diff_drive_node = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "diff_drive_controller",
-        ],
-    )
-    controller_manager = Node(
-        package='controller_manager',
-        executable='ros2_control_node',
-        parameters=[{'robot_description': robot_description}],
-        output='screen'
-    )
-    
-    # gazebo_robot_spawner = Node(
-    #   package='gazebo_ros',
-    #   executable='spawn_entity.py',
-    #   parameters=
-    # )
-    
-    gazebo_robot_spawner = ExecuteProcess(
-            cmd=['ros2', 'service', 'call', '/spawn_entity',
-                 'gazebo_msgs/SpawnEntity', spawn_args]),
-    
-    # node_list.append(joint_state_spawner)
-    # node_list.append(skid_steer_controller)
-    node_list.append(gazebo_robot_spawner[0])
-    node_list.append(diff_drive_node)
-    # node_list.append(controller_manager)
-    
-    # node_list.append(gzclient_cmd)
     launch_gazebo_client = lambda _: [gzclient_cmd]
     
     # Group for RViz and related configurations
@@ -157,8 +167,18 @@ def generate_launch_description():
         ]
     )
     
+    static_transform_publisher = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher',
+            output='screen',
+            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'odom']
+        )
+    
+    # node_list.append(static_transform_publisher)
+    
     node_list.append(rviz_group)
     return LaunchDescription(
       launch_arguments 
-      + node_list
-      + [OpaqueFunction(function=launch_gazebo_client)])
+      + node_list)
+    #   + [OpaqueFunction(function=launch_gazebo_client)])
